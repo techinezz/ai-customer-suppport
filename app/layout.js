6bb6ffc57ab68be08eec4150faecd3c87df1d5fe
techinezz <y.martinez.tech@gmail.com>
2024-08-10 23:21:08 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
-  description: "HEYYY",
+  description: "HEYYYyyy",
 };
 
 export default function RootLayout({ children }) {
